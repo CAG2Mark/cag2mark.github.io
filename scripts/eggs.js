4d744ac9022fb97d62aecd6b8480279526cc3530
CAG2 <markng989@gmail.com>
2020-06-15 10:48:37 +0800

    Added eggs. Sweet

---
+++ scripts/eggs.js
@@ -0,0 +1,19 @@
+// If you are looking in this file
+// Thy memory shall be dumped
+// Into the river that flows
+// Into the lost ocean of forget.
+
+class Student {
+
+    constructor(name, skill) {
+        this.name = name;
+        this.skill = skill;
+    }
+
+}
+
+let me = new Student("Mark Ng", "Coder");
+
+function introduce(person) {
+    alert("You found a future easter egg! Check back later to see this implemented!");
+}
