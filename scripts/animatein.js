9350e5cea8dbf134e5280d0d4db7574aadec31ed
CAG2Mark <markng989@gmail.com>
2020-09-23 10:49:50 +0800

    Updated license of animate-in.js

--- scripts/animatein.js
+++ scripts/animatein.js
@@ -1,13 +1,29 @@
 /* Custom animate-in in JavaScript library */
 
-/* Copyright Mark Ng 2020 */
+/*
 
-/*  
-    You are free to distribute, use and modify this code given that all copies of this code
-    and derivative works only if they are distributed under this license, and all previous
-    contributors including the original copyright holder are be listed in the soruce files. 
-    The copyright holder and contributors shall, in no circumstance, not be held liable for 
-    any damages or losses that incur from the usage of this code or its derivative works.
+MIT License
+
+Copyright (c) 2020 Mark Ng
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 */
 
 /* Variables:
